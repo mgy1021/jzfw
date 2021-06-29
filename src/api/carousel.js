a985327ccb21c159c22a56bd52898dd31a2a903e
彦i <969484687@qq.com>
2021-06-29 11:10:47 +0800

    'wfz提交了轮播管理的api'

--- src/api/carousel.js
+++ src/api/carousel.js
@@ -1,1 +1,25 @@
+/*
+ * @Description:
+ * @Author: wfz
+ * @Date: 2021-06-29 10:38:54
+ * @LastEditors: wfz
+ * @LastEditTime: 2021-06-29 11:09:55
+ */
 // 轮播图配置api
+import { get, post } from '@/utils/request'
+
+export function findAll() {
+  return get('/carousel/findAll')
+}
+
+export function saveOrUpdate(data) {
+  return post('/carousel/saveOrUpdate', data)
+}
+
+export function query(data) {
+  return get('/carousel/query', data)
+}
+
+export function deleteById(data) {
+  return get('/carousel/deleteById', data)
+}
